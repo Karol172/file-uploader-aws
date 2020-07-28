f07357b16113fba675a4a6e0709d42652a69e7f6
Karol Cymerys <karol.cymerys@gmail.com>
2020-07-28 11:47:34 +0200

    established connection with db; liquibase scripts

---
+++ file-uploader-be/src/main/resources/db/changelog/changes/create-file-table-changelog-1.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+-- changeset author:kcymerys
+
+CREATE TABLE FILE
+(
+    ID              BIGINT DEFAULT NEXTVAL('HIBERNATE_SEQUENCE'),
+    FILENAME        VARCHAR(255)        NOT NULL,
+    PATH            VARCHAR(255) UNIQUE NOT NULL,
+    SIZE            BIGINT              NOT NULL,
+    TIMESTAMP       TIMESTAMP           NOT NULL,
+    PRIMARY KEY (ID)
+);
